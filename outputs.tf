98b44c9ea4228e1078cfdf0b582d869ffe26bf91
ankitap1219 <156967746+ankitap1219@users.noreply.github.com>
2024-03-10 23:25:28 +0530

    Update outputs.tf: update output

--- outputs.tf
+++ outputs.tf
@@ -2,5 +2,5 @@
 
 output "public_ip_address" {
     description = "The DNS name of the load balancer"
-    value = "${aws_instance.step-5.public_ip}"
+    value = "${aws_instance.poll_scm.public_ip}"
 }
