ae8328ceb96a0e77204fbd2e4f62ac368db8ee01
ankitap1219 <156967746+ankitap1219@users.noreply.github.com>
2024-04-13 13:22:21 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo yum -y install git
-sudo git clone https://github.com/ankitap1219/step-4.git
-cd step-4/
+sudo git clone https://github.com/ankitap1219/flight-prediction.git
+cd flight-prediction/
 pip3 install -r requirements.txt
 screen -m -d python3 app.py
